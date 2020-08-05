1f3bac7d5b49e62515f344b21b57095baa2ca2e4
Mauro <mauro.spinosi94@gmail.com>
2020-08-05 16:45:06 +0200

    Esercizio 3 (snack3)

--- snack3/js/script.js
+++ snack3/js/script.js
@@ -1,1 +1,8 @@
-document.getElementById('prova').innerHTML = "CIAO"
+// Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo
+var num = parseInt(prompt("Inserisci numero"))
+
+if (num % 2 == 0) {
+  document.getElementById('snack3').innerHTML = num;
+} else {
+  document.getElementById('snack3').innerHTML = num+1;
+}
