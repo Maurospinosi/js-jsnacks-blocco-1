86bf5ba912eb0ce472c90346b2a6e0a43508526e
Mauro <mauro.spinosi94@gmail.com>
2020-08-05 16:04:04 +0200

    Primo esercizio (snack1)

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,1 +1,11 @@
-document.getElementById('prova').innerHTML = "CIAO"
+// l'utente inserisce due numeri in successione, con due prompt.Il software stampa il maggiore
+var primoNumero = prompt("Inserici il primo numero");
+var secondoNumero = prompt("Inserici il secondo numero");
+
+if (primoNumero > secondoNumero) {
+  document.getElementById('snack1').innerHTML = "Il numero" + " " + primoNumero + " " + "è meggiore di" + " " + secondoNumero;
+} else if (secondoNumero > primoNumero) {
+  document.getElementById('snack1').innerHTML = "Il numero" + " " + secondoNumero + " " + "è meggiore di" + " " + primoNumero;
+} else {
+  document.getElementById('snack1').innerHTML = "I due numeri sono uguali";
+}
