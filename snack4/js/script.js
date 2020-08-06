84b6f42a396ddaf6bc713db1a471e617c8098b40
Mauro <mauro.spinosi94@gmail.com>
2020-08-06 12:25:17 +0200

    Esercizio 4 (snack4)

---
+++ snack4/js/script.js
@@ -0,0 +1,8 @@
+//Genera un numero casuale per il pc poi chiedi all’utente di inserire un numero con un prompt infine stampa la somma dei due numeri
+var numeroCasuale = Math.round(Math.random()*100) + 1;
+alert(numeroCasuale);
+// document.getElementById("snack4").innerHTML ="Numero casuale " + numCasuale;
+
+var num = parseInt(prompt("Inserici il numero"));
+
+document.getElementById('snack4').innerHTML = numeroCasuale + num;
