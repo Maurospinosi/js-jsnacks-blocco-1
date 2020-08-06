7d5dae5bbfc08aa10d3250bdb102ca8747c4e546
Mauro <mauro.spinosi94@gmail.com>
2020-08-06 13:25:06 +0200

    Esercizio 5 (snack5)

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -1,6 +1,6 @@
 //Genera un numero casuale per il pc poi chiedi all’utente di inserire un numero con un prompt infine stampa la somma dei due numeri
 var numeroCasuale = Math.round(Math.random()*100) + 1;
-alert(numeroCasuale);
+alert("Numero casuale :" + " " + numeroCasuale);
 // document.getElementById("snack4").innerHTML ="Numero casuale " + numCasuale;
 
 var num = parseInt(prompt("Inserici il numero"));
--- snack5/js/script.js
+++ snack5/js/script.js
@@ -1,1 +1,8 @@
+// genera un numero casuale e poi stampalo se è pari nel box rosso altrimenti in nel box verde
+var num = Math.round(Math.random()*100 ) + 1;
 
+if (num % 2 == 0) {
+  document.getElementById('snack5-r').innerHTML = num;
+} else {
+  document.getElementById('snack5-g').innerHTML = num;
+}
